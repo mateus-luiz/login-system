6fc9ec73f4c8ece3b6171c7a1b10393dc6a067c5
Skullboy <mateus.florentino2001@gmail.com>
2021-05-12 09:09:08 -0300

    Projeto iniciado

---
+++ login-system/LoginSystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace login_system
+{
+    public partial class LoginSystem : Form
+    {
+        public LoginSystem()
+        {
+            InitializeComponent();
+        }
+    }
+}
